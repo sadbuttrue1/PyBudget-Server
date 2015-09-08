f7481fbef69934a60581f8088f685c1b9e83f533
Eugene Aslanov <aslanov.ev@gmail.com>
2015-09-08 16:59:37 +0400

    Wrapped into SSL

--- budget.py
+++ budget.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import sys
+import ssl
 
 from flask import Flask
 from flask.ext.httpauth import HTTPBasicAuth
@@ -12,10 +13,13 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+basedir = os.path.abspath(os.path.dirname(__file__))
+context.load_cert_chain(basedir + '/server.crt', basedir + '/server.key')
 
 from api import *
 
 if __name__ == '__main__':
     if not os.path.exists('db.sqlite'):
         db.create_all()
-    app.run()
+    app.run(ssl_context=context)
--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import os
 __author__ = 'true'
 
 SECRET_KEY = 'you-will-never-guess'
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
