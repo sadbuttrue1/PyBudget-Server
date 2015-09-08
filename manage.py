715906edb63d79e80bbbed667614af2628f8e0fe
Eugene Aslanov <aslanov.ev@gmail.com>
2015-09-08 18:04:59 +0400

    Added migration manager

---
+++ manage.py
@@ -0,0 +1,13 @@
+from flask.ext.migrate import MigrateCommand, Migrate
+from flask.ext.script import Manager
+from budget import app, db
+
+__author__ = 'true'
+
+app.config.from_object('config')
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
