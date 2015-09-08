fd76bea971e39a0f0e415d38b5285f8c65c9736d
Eugene Aslanov <aslanov.ev@gmail.com>
2015-09-08 19:38:07 +0400

    RESTfull api for account

--- api.py
+++ api.py
@@ -31,7 +31,7 @@ def new_user():
 def get_user(id):
     user = User.query.get(id)
     if not user:
-        abort(400)
+        abort(404)
     return jsonify({'username': user.username})
 
 
@@ -81,6 +81,8 @@ def update_account(id):
     name = request.json.get('name')
     info = request.json.get('info')
     account = Account.query.get(id)
+    if not account:
+        abort(404)
     if name is not None:
         account.name = name
     if info is not None:
@@ -90,10 +92,27 @@ def update_account(id):
     return jsonify({'name': account.name}), 201, {'Location': url_for('get_account', id=account.id)}
 
 
+@app.route('/api/accounts/<int:id>', methods=['DELETE'])
+@auth.login_required
+def delete_account(id):
+    account = Account.query.get(id)
+    if not account:
+        abort(404)
+    db.session.delete(account)
+    db.session.commit()
+    return jsonify({'result': True})
+
+
 @app.route('/api/accounts/<int:id>')
 @auth.login_required
 def get_account(id):
     account = Account.query.get(id)
     if not account:
-        abort(400)
+        abort(404)
     return jsonify(account.serialize())
+
+
+@app.route('/api/accounts', methods=['GET'])
+@auth.login_required
+def get_accounts():
+    return jsonify(accounts=[a.serialize() for a in g.user.accounts.all()])
